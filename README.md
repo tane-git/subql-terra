7d6149d4cccfd4df18acc8a90736b9cda9137a30
tane-git <tane.alexander.wilson@gmail.com>
2022-04-20 12:04:35 +1200

    subql init terra starter

---
+++ README.md
@@ -0,0 +1,29 @@
+# SubQuery Starter Project for Terra
+
+This is a starter project for Indexing Terra. It includes a simple blockhandler and an event handler. This project indexes all transfer events in bombay-12.
+
+# Geting Started
+
+### 1. Install dependencies
+
+```shell
+yarn
+```
+
+### 2. Generate types
+
+```shell
+yarn codegen
+```
+
+### 3. Build
+
+```shell
+yarn build
+```
+
+### 4. Run locally
+
+```shell
+yarn start:docker
+```
---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,58 @@
+import { Block, Message, Transaction, TransferEvent } from "../types";
+import {
+  TerraEvent,
+  TerraBlock,
+  TerraMessage,
+  TerraTransaction,
+} from "@subql/types-terra";
+import { MsgExecuteContract } from "@terra-money/terra.js";
+
+export async function handleBlock(block: TerraBlock): Promise<void> {
+  const record = new Block(block.block.block_id.hash);
+  record.height = BigInt(block.block.block.header.height);
+  await record.save();
+}
+
+export async function handleTransaction(tx: TerraTransaction): Promise<void> {
+  const record = new Transaction(tx.tx.txhash);
+  record.blockHeight = BigInt(tx.block.block.block.header.height);
+  record.timestamp = tx.tx.timestamp;
+  await record.save();
+}
+
+export async function handleMessage(
+  msg: TerraMessage<MsgExecuteContract>
+): Promise<void> {
+  const record = new Message(`${msg.tx.tx.txhash}-${msg.idx}`);
+  record.blockHeight = BigInt(msg.block.block.block.header.height);
+  record.txHash = msg.tx.tx.txhash;
+  record.contract = msg.msg.toData().contract;
+  record.sender = msg.msg.toData().sender;
+  record.executeMsg = JSON.stringify(msg.msg.toData().execute_msg);
+  await record.save();
+}
+
+export async function handleEvent(
+  event: TerraEvent<MsgExecuteContract>
+): Promise<void> {
+  const record = new TransferEvent(
+    `${event.tx.tx.txhash}-${event.msg.idx}-${event.idx}`
+  );
+  record.blockHeight = BigInt(event.block.block.block.header.height);
+  record.txHash = event.tx.tx.txhash;
+  for (const attr of event.event.attributes) {
+    switch (attr.key) {
+      case "sender":
+        record.sender = attr.value;
+        break;
+      case "recipient":
+        record.recipient = attr.value;
+        break;
+      case "amount":
+        record.amount = attr.value;
+        break;
+      default:
+    }
+  }
+  await record.save();
+}
